f7a0520c51f45ba926456e56cdc0dae32d0115fd
J Salaski <codebyj@gmail.com>
2022-06-14 19:28:43 -0500

    Crud Project Up to DB link

--- server.js
+++ server.js
@@ -1,32 +1,26 @@
 const express = require('express');
-const { send } = require('express/lib/response');
-const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser'); 
+const MongoClient = require('mongodb').MongoClient
 const app = express();
 const PORT = 3000
-const MongoConnect = require('mongodb').MongoClient
 
-//Body-parser, the urlencoded method tells body-parser to extract data from the form element and add them to the body in the request object
-app.use(bodyParser.urlencoded({ extended: true})) 
+app.use(bodyParser.urlencoded ({ extended: true }))
 
-
-//Express handlers
-
-//Express is listening on PORT
 app.listen(PORT, function(){
     console.log(`listening on ${PORT}`) 
 })
 
-//When a request is made to the root directory of __dirname, serve the file index.html
+//BASIC FORMULA
+//app.get('endpoint', () => {
+    //actions
+//}
+
+//this means when there is a get request of root, send the file index.html from the current directory
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-//This is a create request
+//using body-parser, this 
 app.post('/quotes', (req, res) => {
     console.log(req.body)
 })
-
-MongoClient.connect('mongo-db-connection-string', (err, client => {
-    //.. do something here 
-}
